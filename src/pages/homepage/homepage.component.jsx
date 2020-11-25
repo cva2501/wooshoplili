b16c15b9e5d8a010dd6715796559ba27ded12990
cristian valdivia alvarez <christian_25_alex@hotmail.com>
2020-11-25 03:40:54 -0500

    collection items

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
-const Homepage =( )=> (
+const Homepage =(props )=> (
+    console.log(props),
 <div className='homePage'>
 <Directory />
 
